ac65093106f722788fc43c422aa48fc619001546
xdc1 <709233048@qq.com>
2021-01-14 22:18:10 +0800

    提交TestGit1项目 日期：20210114

---
+++ TestGit1/src/com/test/Test.java
@@ -0,0 +1,26 @@
+package com.test;
+
+public class Test {
+	/**
+	 * 姓名
+	 */
+	private String name;
+	
+	/**
+	 * 年龄
+	 */
+	private int age;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+}
