be0c337489aaf6708ed18f243acc17e2b0bed6b1
xdc1 <709233048@qq.com>
2021-01-15 17:02:12 +0800

    修改Test属性name改为name2，添加MainTest文件；

--- TestGit1/src/com/test/Test.java
+++ TestGit1/src/com/test/Test.java
@@ -4,22 +4,25 @@ public class Test {
 	/**
 	 * 姓名
 	 */
-	private String name;
+	private String name2;
 	
 	/**
 	 * 年龄
 	 */
 	private int age;
 	
-	public String getName() {
-		return name;
+	public String getName2() {
+		return name2;
 	}
-	public void setName(String name) {
-		this.name = name;
+	
+	public void setName2(String name2) {
+		this.name2 = name2;
 	}
+	
 	public int getAge() {
 		return age;
 	}
+	
 	public void setAge(int age) {
 		this.age = age;
 	}
